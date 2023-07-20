15d6be847484d87b532d846afded3c7fcf8bcb39
Rahul T R <r-ravikumar@ti.com>
2023-07-20 16:18:52 +0530

    Makefile/setup: Update gcc to 11.3
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ sudo apt update
 apt install cmake ninja-build
 pip3 install meson
 
-if [ ! -d gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu ]; then
-    wget https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz --no-check-certificate
-    tar xf gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz > /dev/null
-    rm gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz
+if [ ! -d arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu ]; then
+    wget https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz --no-check-certificate
+    tar xf arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz > /dev/null
+    rm arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
 fi
