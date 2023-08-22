cd50635352e55259c0330f71e1e68e8427ccb487
Rahul T R <r-ravikumar@ti.com>
2023-08-22 14:32:23 +0530

    setup: Add support to download targetfs
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- setup.sh
+++ setup.sh
@@ -9,3 +9,12 @@ if [ ! -d arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu ]; then
     tar xf arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz > /dev/null
     rm arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
 fi
+
+if [ ! -d targetfs ]; then
+    wget https://dr-download.ti.com/software-development/software-development-kit-sdk/MD-O34VjGn5mM/09.00.00.08/tisdk-edgeai-image-j721s2-evm.tar.xz
+    mkdir targetfs
+    cd targetfs
+    tar xf ../tisdk-edgeai-image-j721s2-evm.tar.xz > /dev/null
+    cd -
+    rm tisdk-edgeai-image-j721s2-evm.tar.xz
+fi
