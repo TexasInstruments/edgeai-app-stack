facea7438ff911ca8400c0fa2980232b11ecd8df
Rahul T R <r-ravikumar@ti.com>
2024-04-25 10:59:48 +0530

    setup: Use apt to install meson instead of pip
    pip was used to get version > 0.5 in ubuntu 20.04
    Latest ubuntu LTS 22.04 is packaging meson 1.2.0
    so switch to apt
    
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-apt install cmake ninja-build
-pip3 install meson
+apt install cmake ninja-build meson
 
 if [ ! -d arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu ]; then
     wget https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz --no-check-certificate
