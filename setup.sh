03c00de3b6f23052502364341860fbe45bc2fb5f
Rahul T R <r-ravikumar@ti.com>
2024-07-11 20:50:22 +0530

    setup: Update toolchain
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 sudo apt update
 apt install cmake ninja-build meson
 
-if [ ! -d arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu ]; then
-    wget https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz --no-check-certificate
-    tar xf arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz > /dev/null
-    rm arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+if [ ! -d arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-linux-gnu ]; then
+    wget https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz --no-check-certificate
+    tar xf arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz > /dev/null
+    rm arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
 fi
 
 if [ ! -d targetfs ]; then
